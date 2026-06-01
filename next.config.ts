1643eb144668cf0b399b9006da33fed7a144e8bc
rsw <2273357202@qq.com>
2026-06-01 15:37:37 +0800

    fixBug

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ["**/System Volume Information/**", "**/node_modules/**"],
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
